11781f13687786bd81d6019d20899f0bf6ff84a9
chem-eng <michel-bartsch@t-online.de>
2020-11-26 08:10:01 +0000

    Found the Matrix! Add: Calc Biodivers.

--- Task_Day_1.py
+++ Task_Day_1.py
@@ -96,19 +96,69 @@ class MatrixUpdater():
     def get_new_matrix(self):
         self.search_bugs()
         self.search_empty()
-        print("Das ist die Eingangsmatrix")
-        self.print_matrix(self.old_matrix)
-        print("Das ist die neue Matrix")
-        self.print_matrix(self.new_matrix)
+        #print("Das ist die Eingangsmatrix")
+        #self.print_matrix(self.old_matrix)
+        #print("Das ist die neue Matrix")
+        #self.print_matrix(self.new_matrix)
+        return self.new_matrix
 
-        
+class SafeMatrix():
+
+    def __init__(self):
+        self.master_list = []
+
+    def save_matrix(self, matrix):
+        self.master_list.append(matrix)
+         
 
             
+class CompareMatrix():
 
+    def __init__(self):
+        pass
 
+    def compare(self, master_list, current_list):
+        list_counter = 0
+        while list_counter < len(master_list)-1:
+            if current_list[0] == master_list[list_counter][0]:
+                 if current_list[1] == master_list[list_counter][1]:
+                      if current_list[2] == master_list[list_counter][2]:
+                           if current_list[3] == master_list[list_counter][3]:
+                                if current_list[4] == master_list[list_counter][4]:
+                                    return list_counter
+            list_counter += 1
+        return False
+        
+                
+class CalculatBiodivers():
+
+    def __init__(self):
+        pass
+
+    def calc(self,matrix):
+        
 
 if __name__ == "__main__":
-    test = ReadTextFile()
-    initial_matrix = test.file_in_list("task.txt")
-    new_matrix = MatrixUpdater(initial_matrix)
-    new_matrix.get_new_matrix()
+    read_text = ReadTextFile()
+    initial_matrix = read_text.file_in_list("task.txt")
+    #test_matrix = read_text.file_in_list("test_matrix.txt")
+    master = SafeMatrix()
+    master.save_matrix(initial_matrix)
+    compare_matrix = CompareMatrix()
+    old_matrix = initial_matrix
+    first_list = False
+    count = 0
+    while count < 100:
+        new_matrix = MatrixUpdater(old_matrix)
+        master.save_matrix(new_matrix.get_new_matrix())
+        old_matrix = new_matrix.get_new_matrix()
+        first_list = compare_matrix.compare(master.master_list,old_matrix)
+        if first_list is not False:
+            print("Geschafffft")
+            break
+        count += 1
+        print(count)
+    print("##################")
+    print(first_list)
+    print(master)
+
