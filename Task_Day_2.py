e722206639234905deabdf1824dc801df58badd4
chem-eng <michel-bartsch@t-online.de>
2020-11-27 16:38:23 +0000

    Create ReadIn

--- Task_Day_2.py
+++ Task_Day_2.py
@@ -1,1 +1,27 @@
 
+class ReadTextFile():
+    
+    def open_file(self, location):
+        return open(location,"r")
+
+    def read_in_file(self, location):
+        file = self.open_file(location)
+        read_in = file.read()
+        return read_in
+    
+    def creat_list(self, location):
+        read_in = self.read_in_file(location)
+        rare_list = list(read_in)
+        input_list = []
+        for line in range(len(rare_list)):
+            if rare_list[line] != ",":
+                input_list.append(rare_list[line])
+        return input_list
+
+
+
+
+if __name__ == "__main__":
+    test = ReadTextFile()
+    print(test.creat_list("input_day_2.txt"))
+
