3e72ab68845404ba534dd69d5b69792d7485423f
chem-eng <michel-bartsch@t-online.de>
2020-11-30 20:44:43 +0000

    Funktioniert nicht

--- Task_Day_2.py
+++ Task_Day_2.py
@@ -17,11 +17,98 @@ class ReadTextFile():
             if rare_list[line] != ",":
                 input_list.append(rare_list[line])
         return input_list
+    
+    def change_input(self, location):
+        correct_input_list = self.creat_list(location)
+        correct_input_list[1] = "12"
+        correct_input_list[2] = "2"
+        return correct_input_list
+
+class CreatSplitList():
+
+    def __init__(self):
+        self.split_list = []
+        self.input_list = []
+    
+    def set_input_list(self,input_list):
+        self.input_list = input_list
+        
+
+    def create_list(self):
+        elem = 4
+        column = int(len(self.input_list)/4)
+        self.split_list = [[0 for x in range(elem)] for x in range(column)]
+    
+
+    def fill_list(self):
+        count = 0
+        count_2 = 0
+        while count < int(len(self.input_list)/4):
+            count_1 = 0
+            count_2 = 0
+            while count_1 < 4:
+                self.split_list[count][count_1] = self.input_list[count_2]
+                count_2 += 1
+                count_1 += 1
+            count += 1
+
+    def full_cycle(self,input_list):
+        self.set_input_list(input_list)
+        self.create_list()
+        self.fill_list()
+
+
+
+class Move():
+
+    def __init__(self, split_list_object):
+        self.split_list_object = split_list_object
+
+    def play(self, input_list):
+        count = 0
+        print("start")
+        self.split_list_object.full_cycle(input_list)
+        split_list = self.split_list_object.split_list
+        while count < len(input_list)/4 -1:
+            print("count= ")
+            print(count)
+            if self.check_first(split_list[count][0]) == "Add":
+                input_list[int(split_list[count][3])] = self.add_value(input_list[int(split_list[count][1])], input_list[int(split_list[count][2])])
+            if self.check_first(split_list[count][0]) == "multi":
+                input_list[split_list[count][3]] = self.multiply_value(input_list[split_list[count][1]], input_list[split_list[count][2]])
+            if self.check_first(split_list[count][0]) == "end":
+                print("FERTIG")  
+            count += 1
+            self.split_list_object.full_cycle(input_list)
+            split_list = self.split_list_object.split_list
+        return input_list[0]
+
+
+    def check_first(self, value):
+        if value == "1":
+            return "Add"
+        if value == "2":
+            return "multi"
+        if value == "99":
+            return "end"
+        else:
+            return False
+    
+
+    def add_value(self, first, second):
+        return first + second
+    
+    def multiply_value(self, first, second):
+        return first * second
+
 
 
 
 
 if __name__ == "__main__":
-    test = ReadTextFile()
-    print(test.creat_list("input_day_2.txt"))
-
+    input_value = ReadTextFile()
+    input_list = input_value.change_input("input_day_2.txt")
+    print(len(input_list))
+    split_list_object = CreatSplitList()
+    move = Move(split_list_object)
+    print(move.play(input_list))
